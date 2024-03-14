f0692ad4928d69547267b4c8942f93f9ff3cf74a
Rudys Natanael Acosta Crousset <ncrousset926@gmail.com>
2024-03-13 23:29:26 -0400

    enlaces creados

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('/pages/home.html')
 
+@app.route('/what-i-do')
+def what_i_do():
+    return render_template('/pages/what-i-do.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('/pages/projects.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('/pages/blog.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8001, debug=True)
